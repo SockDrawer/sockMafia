349974aa75e21d0dac8a71f2d3a237e199c32041
Yami <yamikuronue@gmail.com>
2016-02-17 18:16:39 -0500

    Restore set handler

--- src/mafiabot.js
+++ src/mafiabot.js
@@ -105,6 +105,7 @@ function registerModCommands(events) {
 	events.onCommand('start', 'move a game into active play (mod only)', exports.startHandler, handleCallback);
 	events.onCommand('new-day', 'move on to a new day (mod only)', exports.dayHandler, handleCallback);
 	events.onCommand('kill', 'kill a player (mod only)', exports.killHandler, handleCallback);
+	events.onCommand('set', 'Assign a player a role (mod only)', exports.setHandler, handleCallback);
 	events.onCommand('end', 'end the game (mod only)', exports.finishHandler, handleCallback);
 }
 
