1a670e5286d08ff0c34b203cb63f6b5856c407e1
Yami <yamikuronue@gmail.com>
2016-11-02 13:57:13 +0000

    chore: move conf.py?

--- docs/conf.py
+++ docs/conf.py
@@ -4,18 +4,18 @@ from os.path import join, getsize
 
 os.system('npm install')
 
-for root, dirs, files in os.walk('src'):
+for root, dirs, files in os.walk('../src'):
 	for name in files:
 		if name.endswith('.js'):
 			docname = os.path.join(root, name.replace('.js', '.md'))
 			try:
-				os.makedirs(os.path.join('docs', root))
+				os.makedirs(os.path.join('./', root))
 				break
 			except OSError, e:
 				if e.errno != 17:
 					raise   
 				pass
-			os.system('jsdoc2md ' + os.path.join(root, name) + ' > docs/' + docname)
+			os.system('jsdoc2md {1} > ./{2}'.format(os.path.join(root, name), docname))
 
 source_parsers = {
     '.md': CommonMarkParser,
