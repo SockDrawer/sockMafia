ea0d323645c8dd1a4d2d0f8e0339ad363001f732
Accalia de Elementia <accalia.de.elementia@gmail.com>
2017-04-04 12:16:26 +0000

    chore(build): Update documentaion build scripts

--- build/build_docs.py
+++ build/build_docs.py
@@ -3,15 +3,17 @@ from os.path import join, getsize
 
 os.system('npm install jsdoc-to-markdown')
 
-for root, dirs, files in os.walk('../src'):
+for root, dirs, files in os.walk('./src'):
 	for name in files:
 		if name.endswith('.js'):
-			docname = os.path.join(root, name.replace('.js', '.md'))
+			dest = os.path.join('docs/', root)
+			docname = os.path.join(dest, name.replace('.js', '.md'))
 			try:
-				os.makedirs(os.path.join('./', root))
+				os.makedirs(dest)
 				break
 			except OSError, e:
 				if e.errno != 17:
-					raise   
+					raise
 				pass
+			print(docname)
 			os.system('jsdoc2md {0} > ./{1}'.format(os.path.join(root, name), docname))
